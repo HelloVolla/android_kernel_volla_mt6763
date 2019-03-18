dd56712dc0889145bdfe67dcd1904e35b278b088
NeKit <nekit1000@gmail.com>
2019-03-18 22:02:40 +0100

    (hybris) always boot to initramfs

--- init/initramfs.c
+++ init/initramfs.c
@@ -612,7 +612,9 @@ static int __init skip_initramfs_param(char *str)
 {
 	if (*str)
 		return 0;
-	do_skip_initramfs = 1;
+	// Halium: always boot to initramfs
+	//do_skip_initramfs = 1;
+	do_skip_initramfs = 0;
 	return 1;
 }
 __setup("skip_initramfs", skip_initramfs_param);
