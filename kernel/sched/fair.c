4819a8b2e2c4e9511304ab6c40409f5dba079614
Tyler Nijmeh <tylernij@gmail.com>
2020-06-12 18:07:09 -0700

    sched: Increase the time a task is considered cache-hot
    According to RedHat, increasing this tunable reduces the number of
    task migrations. This should reduce time spent balancing tasks and
    increase per-task performance.
    
    https://www.redhat.com/files/summit/session-assets/2018/Performance-analysis-and-tuning-of-Red-Hat-Enterprise-Linux-Part-1.pdf
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -109,7 +109,7 @@ unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
 
 const_debug unsigned int sched_cache_hot_migration_cost = 500000UL;
-const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 1000000UL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
