36fd6c291be0ee2a7a062baca1eff5ccc40f67e3
Alfred Neumayer <dev.beidl@gmail.com>
2020-11-19 03:56:39 +0100

    rt.c: Avoid using rt_rq_throttled() to determine rescheduling
    This reflects the upstream kernel behavior.
    
    Change-Id: Iaa176ed916aab3b5494bf1f8785df174e10c49bf

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2079,7 +2079,9 @@ static int push_rt_task(struct rq *rq)
 	struct task_struct *next_task;
 	struct rq *lowest_rq;
 	int ret = 0;
+#ifdef CONFIG_RT_GROUP_SCHED
 	struct rt_rq *rt_rq;
+#endif
 
 	if (!rq->rt.overloaded)
 		return 0;
@@ -2103,11 +2105,15 @@ retry:
 	 * just reschedule current.
 	 */
 	if (unlikely(next_task->prio < rq->curr->prio)) {
+#ifdef CONFIG_RT_GROUP_SCHED
 		/* We only reschedule when next_task not throttle */
 		if (!rt_rq_throttled(rt_rq)) {
+#endif
 			resched_curr(rq);
 			return 0;
+#ifdef CONFIG_RT_GROUP_SCHED
 		}
+#endif
 	}
 
 	/* We might release rq lock */
