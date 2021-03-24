6093dfbc86318db0f85ec17b6a65639129ab4cd3
Erfan Abdi <erfangplus@gmail.com>
2021-03-24 21:42:27 +0430

    tz_driver: Don’t wait for uTdriver TAs ready signal

--- drivers/misc/mediatek/teei/280/tz_driver/teei_client_main.c
+++ drivers/misc/mediatek/teei/280/tz_driver/teei_client_main.c
@@ -958,7 +958,7 @@ static int init_teei_framework(void)
 	TEEI_BOOT_FOOTPRINT("TEEI BOOT Keymaster Unlocked");
 
 	/* android notify the uTdriver that the TAs is ready !*/
-	wait_for_completion(&boot_decryto_lock);
+	// wait_for_completion(&boot_decryto_lock);
 	TEEI_BOOT_FOOTPRINT("TEEI BOOT Decrypt Unlocked");
 
 	retVal = teei_service_init_second();
