af892d289248fa6400aaa90986f3344ce0eef864
Alfred Neumayer <dev.beidl@gmail.com>
2021-12-14 19:43:42 +0100

    binder: Make global PID lookups configurable
    This makes it possible to:
    - Disable the feature at will of the kernel maintainer
    - Detect from user space via the module parameter whether
      the feature is enabled or not

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -216,6 +216,9 @@ static int binder_set_stop_on_user_error(const char *val,
 module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 	param_get_int, &binder_stop_on_user_error, 0644);
 
+static bool binder_global_pid_lookups = true;
+module_param_named(global_pid_lookups, binder_global_pid_lookups, bool, S_IRUGO);
+
 #define binder_debug(mask, x...) \
 	do { \
 		if (binder_debug_mask & mask) \
@@ -5206,7 +5209,7 @@ retry:
 			trd->sender_pid =
 				task_tgid_nr_ns(sender,
 						task_active_pid_ns(current));
-			if (trd->sender_pid == 0)
+			if (binder_global_pid_lookups && trd->sender_pid == 0)
 				trd->sender_pid = task_tgid_nr(sender);
 		} else {
 			trd->sender_pid = 0;
